8e138a02dfdc8cab2554c36a4b82633838d4ca59
Mrityunjay <mrityunjayvyasmhl@gmail.com>
2021-11-26 18:58:47 +0530

    routerAdded

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
     proxyRouter: {
         'app1.localhost:3000': 'https://www.google.com',  // host only
         'harsh.localhost:3000': 'https://harshsinghvi.com',  // host only
+        'mrityunjay.localhost:3000': 'https://mrityunjayproject.netlify.app/',  // host only
     },
     defaultRouterPath: 'http://www.example.org'
 }
