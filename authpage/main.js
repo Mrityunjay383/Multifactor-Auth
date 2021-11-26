74bc0248667391c5a72fbb5f787a79b177be54fa
Harsh <harshsinghvi98@gmail.com>
2021-11-26 10:52:52 +0530

    Update auth page

--- authpage/main.js
+++ authpage/main.js
@@ -1,41 +1,43 @@
 const CB_URL = getQueryVariable("CB_URL");
 
+// TODO CONFIG OKTA Client
+const signIn = new OktaSignIn({ baseUrl: CB_URL });
+
 const mockLogin = () => {
     setCookie('auth', 'RANDOM STRING', 1)
     console.log(getCookie('auth'))
     window.location.replace(CB_URL);
 }
 
-const main = () => {
-    // TODO CONFIG OKTA Client
-    const signIn = new OktaSignIn({ baseUrl: CB_URL });
-
-    window.onload = (() => {
-        signIn.renderEl(
-            {
-                el: "#widget-container",
-            },
-            function success(res) {
-                if (res.status === "SUCCESS") {
-                    console.log(
-                        "Do something with this sessionToken",
-                        res.session.token
-                    );
-
-                    // TODO set Cookie for auth token
-                    setCookie('auth', res.session.token, 1)
-                    
-                    // TODO Rerdirect if needed
-                    // window.location.replace(CB_URL);
-
-                } else {
-                    // The user can be in another authentication state that requires further action.
-                    // For more information about these states, see:
-                    //   https://github.com/okta/okta-signin-widget#rendereloptions-success-error
-                }
-            }
-        );
-    })
+const oktaSigninRenderOptions = {
+    el: "#widget-container",
 }
 
-main();
+const oktaAuthSuccessHandler = (res) => {
+    if (res.status === "SUCCESS") {
+        console.log(
+            "Do something with this sessionToken",
+            res.session.token
+        );
+
+        // TODO set Cookie for auth token
+        setCookie('auth', res.session.token, 1)
+
+        // TODO Rerdirect if needed
+        // window.location.replace(CB_URL);
+
+    } else {
+        // The user can be in another authentication state that requires further action.
+        // For more information about these states, see:
+        //   https://github.com/okta/okta-signin-widget#rendereloptions-success-error
+    }
+}
+
+// To manually display signin widget
+const oktaLoginPopup = () => {
+    signIn.renderEl(oktaSigninRenderOptions, oktaAuthSuccessHandler);
+}
+
+window.onload = (() => {
+    oktaLoginPopup()
+})
